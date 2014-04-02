d72dd20269b4ae5c95b2350d02f2d1e4ce16ec82
Damian Beresford <damian.beresford@feedhenry.com>
2014-04-02 11:43:31 +0100

    6593 - Add basic turbo test to fh-cloud-mbaas-instance-blank: checked in test directory

---
+++ test/unit/test-main.js
@@ -0,0 +1,23 @@
+var proxyquire = require('proxyquire');
+var assert = require('assert');
+var util = require('util');
+
+// Sample setUp call - note this is called once, before the other tests in this file are run
+exports.setUp = function(finish) {
+  finish();
+};
+
+// Sample tearDown - note this is called once, after all other tests have been run in this file
+exports.tearDown = function(finish) {
+  finish();
+};
+
+exports.it_should_test_happy = function(finish) {
+
+  var main = proxyquire('lib/main.js', {});
+  main.hello({hello: 'world'}, function(err, data) {
+    assert.ok(!err, 'Unexpected error: ' + util.inspect(err));
+    assert.equal(data.msg, 'Hello world', 'Unexpected response! - ' + util.inspect(data));
+    finish();
+  });
+};
