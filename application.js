2afeb43adb27adfd9d454e3cfa7de16574324ff1
Damian Beresford <damian.beresford@feedhenry.com>
2014-04-07 11:13:55 +0100

    Use new fh-webapp error handler

--- application.js
+++ application.js
@@ -13,6 +13,9 @@ app.use('/', function(req, res){
   res.end('Your Cloud App is Running');
 });
 
+// Important that this is last!
+app.use(webapp.errorHandler());
+
 var port = process.env.FH_PORT || process.env.VCAP_APP_PORT || 8001;
 module.exports = app.listen(port, function(){
   console.log("App started at: " + new Date() + " on port: " + port);
