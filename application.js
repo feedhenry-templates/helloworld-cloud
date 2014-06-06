023f2a01aeb2343aa26cdeb1f923b3b3cbabd7a7
Jason Madigan <jason.madigan@feedhenry.com>
2014-06-06 09:37:22 +0100

    Adding CORS middleware for all requests

--- application.js
+++ application.js
@@ -1,10 +1,14 @@
 var mbaas = require('fh-mbaas-express');
 var express = require('express');
+var cors = require('cors');
 
 // Securable endpoints: list the endpoints which you want to make securable here
 var securableEndpoints = ['hello'];
 
 var app = express();
+
+// Enable CORS for all requests
+app.use(cors());
 
 // Note: the order which we add middleware to Express here is important!
 app.use('/sys', mbaas.sys(securableEndpoints));
@@ -16,7 +20,7 @@ app.use(mbaas.fhmiddleware());
 app.use('/hello', require('./lib/hello.js')());
 
 // You can define custom URL handlers here, like this one:
-app.use('/', function(req, res){
+app.use('/', function(req, res) {
   res.end('Your Cloud App is Running');
 });
 
@@ -24,6 +28,6 @@ app.use('/', function(req, res){
 app.use(mbaas.errorHandler());
 
 var port = process.env.FH_PORT || process.env.VCAP_APP_PORT || 8001;
-var server = app.listen(port, function(){
+var server = app.listen(port, function() {
   console.log("App started at: " + new Date() + " on port: " + port);
 });
